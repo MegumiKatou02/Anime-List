44aaada452c7ed64b5c483ec5c42fe91c5a31ccd
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-02-26 13:03:55 +0700

    feat: remove console log from MangaService and add default settings interface

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,6 +1,7 @@
 import { ref } from 'vue'
+import { defaultSettings } from './constants'
 
-const savedSettings = JSON.parse(localStorage.getItem('setting') || `{"lang":"0","theme":"0"}`)
+const savedSettings = JSON.parse(localStorage.getItem('setting') || JSON.stringify(defaultSettings))
 const isDarkTheme = savedSettings.theme == '0' ? true : false
 
 export const isDarkMode = ref(isDarkTheme)
