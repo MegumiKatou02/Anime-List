1d2505ca9835f1ab0493f4e480d387379fd4ce8b
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-03-10 12:19:26 +0700

    feat: integrate Cloudflare Turnstile for enhanced login security

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { createApp } from 'vue'
 import { createHead } from '@vueuse/head'
 import App from './App.vue'
 import router from './router'
+import TurnstileWidget from 'vue-turnstile'
 
 const app = createApp(App)
 const head = createHead()
 
 app.use(router)
 app.use(head)
+app.component('turnstile-widget', TurnstileWidget)
 
 app.mount('#app')
