0e7ae720cfbac89c9f5dbab62b5feaa35bac0f06
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-03-12 19:59:36 +0700

    fix: add Pinia state management library to the project

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createHead } from '@vueuse/head'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import TurnstileWidget from 'vue-turnstile'
 
 const app = createApp(App)
 const head = createHead()
+const pinia = createPinia()
 
 app.use(router)
 app.use(head)
+app.use(pinia)
 app.component('turnstile-widget', TurnstileWidget)
 
 app.mount('#app')
