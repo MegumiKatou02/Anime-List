4fce8675e4ae05fa096013180720bfa82d148980
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-03-19 23:55:32 +0700

    feat: add bayesian rating to RatingDistribution component

--- src/components/RatingDistribution.vue
+++ src/components/RatingDistribution.vue
@@ -51,6 +51,7 @@ export default defineComponent({
       type: Object as () => {
         rating?: {
           distribution?: Record<string, number>
+          bayesian?: number
         }
       },
       required: true,
